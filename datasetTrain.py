9a4d6c3c97c26c4faf5280352d4454a2fbe8194f
nuwanthaDilshan <nuwanthadilshan59@gmail.com>
2022-09-24 11:40:49 +0530

    Import libraries that need

---
+++ datasetTrain.py
@@ -0,0 +1,10 @@
+#import libraries
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+import random
+import string
+import numpy as np
+import nltk
+import warnings
+warnings.filterwarnings("ignore")
+
