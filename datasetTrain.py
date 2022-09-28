f45cd2e3ebf4d57f32a8cb410dbd1f1818ba69bf
nuwanthaDilshan <nuwanthadilshan59@gmail.com>
2022-09-28 15:07:19 +0530

    dataset open

--- datasetTrain.py
+++ datasetTrain.py
@@ -1,4 +1,6 @@
 #import libraries
+from curses import window
+from tkinter import Frame
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 import random
@@ -8,3 +10,4 @@ import nltk
 import warnings
 warnings.filterwarnings("ignore")
 
+f = open('dataset.txt', 'r', errors='ignore')
