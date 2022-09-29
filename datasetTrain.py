8b413d3ba7db2a70fb04e549beb18ff994184ee0
nuwanthaDilshan <nuwanthadilshan59@gmail.com>
2022-09-29 10:42:15 +0530

    Text normalization

--- datasetTrain.py
+++ datasetTrain.py
@@ -1,5 +1,6 @@
 #import libraries
 from curses import window
+from lib2to3.pgen2 import token
 from tkinter import Frame
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -23,3 +24,11 @@ sent_tokens[:2]
 word_tokens[:5]
 
 lemmer = nltk.stem.WordNetLemmatizer() #Text normalization
+
+def LemTokens(tokens):
+    return[lemmer.lemmatize(token)for token in tokens]
+
+remove_punct_dict = dict((ord(punct), None)for punct in string.punctuation)
+
+def LemNormalize(text):
+    return LemNormalize(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
