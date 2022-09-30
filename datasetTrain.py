890053064802520b0e1aee973612542ee1d5acc6
nuwanthaDilshan <nuwanthadilshan59@gmail.com>
2022-09-30 15:44:08 +0530

    Coded the frontend and connected it

--- datasetTrain.py
+++ datasetTrain.py
@@ -28,7 +28,7 @@ def LemTokens(tokens):
 remove_punct_dict = dict((ord(punct), None)for punct in string.punctuation)
 
 def LemNormalize(text):
-    return LemNormalize(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
 
 
 Introduce_Ans = ["My name is AgroBot.", "My name is AgroBot you can called me Agro.", "Im AgroBot :) ",
@@ -37,6 +37,12 @@ GREETING_INPUTS = ("hello", "hi", "hiii", "hii", "hiiii",
                    "hiiii", "greetings", "sup", "what's up", "hey",)
 GREETING_RESPONSES = ["hi", "hey", "hii there",
                       "hi there", "hello", "I am glad! You are talking to me"]
+Basic_Q_1 = ("please help", "help me","please help me","can you help me")
+Basic_Ans_1 = ["yes I can", "Tell me", "sure"]
+Basic_Q_2 = ("Okay", "okay","ok")
+Basic_Ans_2 = ["Is there anything else to know?"]
+Basic_Q_3 = ("no", "No","not yet", "Not yet","Nop","nop")
+Basic_Ans_3 = ["Okay Thank you Bye", "Have a nice day", "Bye"]
 
 
 #Checking for greetings
@@ -51,6 +57,26 @@ def IntroduceMe(sentence):
     return random.choice(Introduce_Ans)
 
 
+# Checking for Basic_Q_1
+def basic1(sentence):
+    for word in Basic_Q_1:
+        if sentence.lower() == word:
+            return random.choice(Basic_Ans_1)
+
+# Checking for Basic_Q_2
+def basic2(sentence):
+    for word in Basic_Q_2:
+        if sentence.lower() == word:
+            return random.choice(Basic_Ans_2)
+
+
+# Checking for Basic_Q_3
+def basic3(sentence):
+    for word in Basic_Q_3:
+        if sentence.lower() == word:
+            return random.choice(Basic_Ans_3)
+
+
 #Generating response
 def response(user_response):
     bot_response = ''
@@ -59,7 +85,7 @@ def response(user_response):
     tfidf = TfidfVec.fit_transform(sent_tokens)
     vals = cosine_similarity(tfidf[-1], tfidf)
     idx = vals.argsort()[0][-2]
-    flat = vals.flatter()
+    flat = vals.flatten()
     flat.sort()
     req_tfidf = flat[-2]
     if(req_tfidf == 0):
@@ -68,3 +94,30 @@ def response(user_response):
     else:
         bot_response = bot_response + sent_tokens[idx]
         return bot_response
+
+
+def Agro(user_response):
+    user_response = user_response.lower()
+    keyword = " module "
+    keywordone = " module"
+    keywordsecond = "module "
+
+    if (user_response != 'bye'):
+        if (user_response == 'thanks' or user_response == 'thank you'):
+            return "You are welcome.."
+        else:
+            if (greeting(user_response) != None):
+                return greeting(user_response)
+            elif (user_response.find("your name") != -1 or user_response.find(" your name") != -1 or user_response.find("your name ") != -1 or user_response.find(" your name ") != -1):
+                return IntroduceMe(user_response)
+            elif (basic1(user_response) != None):
+                return basic1(user_response)
+            elif (basic2(user_response) != None):
+                return basic2(user_response)
+            elif (basic3(user_response) != None):
+                return basic3(user_response)
+            else:
+                return response(user_response)
+
+    else:
+        return "Bye! take care.."
