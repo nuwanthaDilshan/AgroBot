8b36c347fea8e04e846c77164f63a227d1084b0e
nuwanthaDilshan <nuwanthadilshan59@gmail.com>
2022-10-09 10:03:04 +0530

    add test file and same functions tested

--- chatBot_test.py
+++ chatBot_test.py
@@ -1,36 +1,17 @@
-from chatBot import basic1, basic2, greeting, IntroduceMe
+import unittest
+import chatBot
 
-# greeting test
-def test_greeting():
-    assert greeting("hi") == "hello"
+class test(unittest.TestCase):
 
+    def test_greeting(self):
+        self.assertEqual(chatBot.greeting("hi"), "hey")
 
-if __name__ == "__main__":
-    test_greeting()
-    print("Everything passed")
-
-# IntroduceMe test
-def test_IntroduceMe():
-    assert IntroduceMe("your name") == "My name is AgroBot."
+    def test_IntroduceMe(self):
+        self.assertEqual(chatBot.IntroduceMe(
+            "your name"), "My name is AgroBot.")
 
 
 if __name__ == "__main__":
-    test_IntroduceMe()
-    print("Everything passed")
+    unittest.main()
 
-# Basic_Q_1 test
-def test_Basic_Q_1():
-    assert basic1("can you help me") == "yes I can"
-
-if __name__ == "__main__":
-    test_Basic_Q_1()
-    print("Everything passed")
-
-# Basic_Q_2 test
-def test_Basic_Q_2():
-    assert basic2("Okay") == "Is there anything else to know?"
-
-if __name__ == "__main__":
-    test_Basic_Q_2()
-    print("Everything passed")
 
